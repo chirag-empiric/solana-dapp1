891af91aad5cf7f184b32d8b7ce9b31461d104a8
chirag-empiric <chirag@empiricinfotech.com>
2024-07-30 11:46:57 +0530

    feat: initial take part setup and pick winner setup, CPI next

--- rustshi/programs/rustshi/src/lib.rs
+++ rustshi/programs/rustshi/src/lib.rs
@@ -1,51 +1,111 @@
 use anchor_lang::prelude::*;
 
-declare_id!("CRjWCs644dmTa2BQjWW5AjvCkRpwmPtmF55CR6t4GmBG");
+declare_id!("AGHt9cGqRDo1dokwpXMTbRwrJzgqTmuCfLJKBG28cDv6");
 
 #[program]
-pub mod rustshi {
+pub mod simplepred {
     use anchor_lang::solana_program::{program::invoke, system_instruction::transfer};
 
     use super::*;
 
-    pub fn participate(mut ctx: Context<MainDataSource>, amount: u64) -> Result<()> {
-        let cont = &mut ctx.accounts;
+    pub fn part_bet(mut ctx: Context<GlobalContext>, amt: u64, choice: Choice) -> Result<()> {
+        let context = &mut ctx.accounts;
 
+        let user_bet = Bet {
+            bet_amount: amt,
+            user_choice: choice, // passing true false from the arg itself
+            p_key: context.signer.key(),
+        };
+        context.global_state.userlist.push(user_bet);
+        context.global_state.total_betting_amount += amt;
+
+        // token transfer shit here - learn CPI for that (transferFrom type shi prolly)
+        let reward_pool = 90 * amt / 100;
+        // let platform = 7 * amt / 100;
+        // let burn_pool = 3 * amt / 100;
+
+        // 90% rewards pool
         invoke(
-            &transfer(&cont.signer.key(), &cont.hits.key(), amount),
-            &[cont.signer.to_account_info(), cont.hits.to_account_info()],
+            &transfer(
+                &context.signer.key(),
+                &context.reward_pool.key(),
+                reward_pool,
+            ),
+            &[
+                context.signer.to_account_info(),
+                context.reward_pool.to_account_info(),
+            ],
         )?;
 
-        let mut user_exist = false;
+        // 7%  platform
+        // 3%  burnt
+        Ok(())
+    }
 
-        for (pos, ele) in cont.hits.user_list.clone().iter().enumerate() {
-            if *ele == cont.signer.key() {
-                cont.hits.user_deposits[pos] += amount;
-                user_exist = true;
-                break;
+    pub fn declare_result(ctx: Context<FinalContext>, winning_choice: Choice) -> Result<()> {
+        let final_state = &mut ctx.accounts.final_state;
+        let global_state = &ctx.accounts.global_state;
+
+        final_state.correct_choice = winning_choice;
+
+        // pick list of winners somehow
+        let mut winners: Vec<Pubkey> = Vec::new();
+        for bet in &global_state.userlist {
+            if bet.user_choice.clone() == winning_choice.clone() {
+                winners.push(bet.p_key);
             }
         }
 
-        if user_exist == false {
-            cont.hits.user_list.push(cont.signer.key());
-            cont.hits.user_deposits.push(amount);
-        }
+        final_state.winning_user_address = winners;
 
         Ok(())
     }
 }
 
+#[derive(Clone, Copy, AnchorDeserialize, AnchorSerialize, PartialEq, Eq)]
+pub enum Choice {
+    Trump(bool),
+    Kamala(bool),
+    Obama(bool),
+    Modiji(bool),
+}
+
+#[derive(Clone, AnchorDeserialize, AnchorSerialize)]
+pub struct Bet {
+    p_key: Pubkey,
+    bet_amount: u64,
+    user_choice: Choice,
+}
+
 #[account]
-pub struct Hits {
-    user_list: Vec<Pubkey>,
-    user_deposits: Vec<u64>,
-    hits: u16,
+pub struct GlobalState {
+    userlist: Vec<Bet>,
+    total_betting_amount: u64,
+}
+
+#[account]
+pub struct FinalState {
+    correct_choice: Choice,
+    winning_user_address: Vec<Pubkey>,
 }
 
 #[derive(Accounts)]
-pub struct MainDataSource<'info> {
-    #[account(init_if_needed, payer = signer, space= 32 * 8 + 2 + 8, seeds = [b"helo"], bump)]
-    hits: Account<'info, Hits>,
+pub struct GlobalContext<'info> {
+    #[account(init_if_needed, payer = signer, space = (32 + 8 + 1) * 100 + 8, seeds = [b"jua"], bump)]
+    global_state: Account<'info, GlobalState>,
+    #[account(mut)]
+    signer: Signer<'info>,
+    #[account(mut, seeds = [b"khatu"], bump)]
+    reward_pool: AccountInfo<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct FinalContext<'info> {
+    #[account(init_if_needed, payer = signer, space = (32 + 8 + 1) * 100 + 8, seeds = [b"juari"], bump)]
+    final_state: Account<'info, FinalState>,
+    #[account(mut, seeds = [b"jua"], bump)]
+    global_state: Account<'info, GlobalState>,
     #[account(mut)]
     signer: Signer<'info>,
     system_program: Program<'info, System>,
